b805a1d9cdf29250ce1ed354ceea21299eef5908
bosquillonde <armand.bosquillon@student.uclouvain.be>
2015-03-19 21:03:52 +0100

    boxplot

--- RPart3.R
+++ RPart3.R
@@ -51,7 +51,7 @@ samplingTest <- function() {
       trainingvector <- randomvector[1:len]
       mysample <- DigitsTrain[trainingvector,]
       
-      model <- svm(Class ~ .,DigitsTrain ,cost = 4, gamma = 0.0004)#0.0004###TODO
+      model <- svm(Class ~ .,mysample ,cost = 4, gamma = 0.0004)#0.0004###TODO
       pre.test <- predict(model, DigitsTest)
       acc[i] = acc[i] + accuracy(table(pred = pre.test , true = t(DigitsTest[1])))/num
             
@@ -66,13 +66,37 @@ samplingTest <- function() {
 df = samplingTest()
 df
 
-data <- data.frame(percent = df[1:10,4], 
-                   percent = df[11:20,4],
-                   percent = df[21:30,4],
-                   percent = df[31:40,4],
-                   percent = df[41:50,4],
-                   percent = df[51:60,4]
+data <- data.frame(percent = df[1:10,1], 
+                   percent = df[11:20,1],
+                   percent = df[21:30,1],
+                   percent = df[31:40,1],
+                   percent = df[41:50,1],
+                   percent = df[51:60,1]
 )
+boxplot(data)
+
+vector <- c(2, 5, 10, 20, 50, 99)/100
+acc = c()
+num = 10
+df <- data.frame()
+for ( i in 1:length(vector)) {
+  for ( j in 1:num) {
+    dim <- dim(DigitsTrain)
+    randomvector <- sample(dim[1])
+    len = (dim[1]*vector[i])
+    trainingvector <- randomvector[1:len]
+    mysample <- DigitsTrain[trainingvector,]
+    
+    model <- svm(Class ~ .,DigitsTrain ,cost = 4, gamma = 0.0004)#0.0004###TODO
+    pre.test <- predict(model, DigitsTest)
+    acc[i] = acc[i] + accuracy(table(pred = pre.test , true = t(DigitsTest[1])))/num
+    accuracy(table(pred = pre.test , true = t(DigitsTest[1])))
+    df[(i-1)*num+j,1] = accuracy(table(pred = pre.test , true = t(DigitsTest[1])))
+  }
+}
+plot(vector, acc, xlab="size", ylab="accuracy", type="l", col="blue")
+#lines(vector, size, xlab="size", ylab="number of nodes", type="l", col="blue")
+df
 
 # 4)
 ##### Choose the training examples uniformly at random
